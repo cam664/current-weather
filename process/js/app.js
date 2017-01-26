5ae124a9d9f9c74526a1fe75d1317bfa616af4f8
cam664 <c.laing@live.com>
2017-01-26 13:01:10 -0500

    fix day and night icons always showing night. Add submit button for new city

--- process/js/app.js
+++ process/js/app.js
@@ -1,11 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function () {
     data.getLoc();
 });
 
-var data = (function(){
+var data = (function () {
     //lookup users location, always calls getWeather() and passes it the city location
     function getLoc() {
-        $.get('http://ip-api.com/json', function(response) {
+        $.get('http://ip-api.com/json', function (response) {
             data.getWeather(response.city);
         })
     };    
@@ -31,8 +31,8 @@ var view = (function(){
     var $wind = $('#wind-data');
     var $humid = $('#humid-data');
     var $city = $('#city-data');
-    var $icon = $('#icon').find('i');    
-    
+    var $icon = $('#icon').find('i'); 
+
     function render(data) {
         var descCapitalize = data.weather[0].description.substring(0,1).toUpperCase();//openweathermap doesn't capitalize the first letter of it's description annoyingly
         
@@ -43,7 +43,7 @@ var view = (function(){
         $wind.html(data.wind.speed);
         $humid.html(data.main.humidity);
         $city.html(data.name +', '+ data.sys.country);
-        if(Date.now() > data.sys.sunrise && Date.now() < data.sys.sunset) {//for 'moon' and 'sun' icons, respectively
+        if(Date.now().toString().slice(0,10) > data.sys.sunrise && Date.now().toString().slice(0,10) < data.sys.sunset) {//for 'moon' and 'sun' icons respectively. Use only first 9 numbers of Date.now() because API only uses 9 numbers.
             $icon.removeClass().addClass('wi wi-owm-day-'+data.weather[0].id+'');
         } else {
             $icon.removeClass().addClass('wi wi-owm-night-'+data.weather[0].id+'');
@@ -57,25 +57,30 @@ var view = (function(){
 
 var search = (function(){
     
+    var $newCityForm = $('#new-city-form');
+    var $newCityBtn = $('#new-city-btn');
+    
     bindEvents();
 
-    var form = document.querySelector('#new-city');//not using jquery in order to use .value
+    var form = document.querySelector('#new-city-form');//not using jquery in order to use .value
 
     function bindEvents() { 
-        $('#new-city').on('focus', function(){
+        $newCityForm.on('focus', function () {
            form.value = ''; 
         });
-        
-        $('#new-city').on('blur', function(){
-           form.value = 'New City'; 
-        });
-        
-        $(window).on('keyup', function(e){
+
+        $(window).on('keyup', function (e) {
             var city = form.value;
-            
+
             if(e.keyCode == 13) {//if it's enter, query open weather map with the input city name
                 data.getWeather(city);
             }
-        })
-    };
+        });
+        
+        $newCityBtn.on('click', function (e) {
+            var city = form.value;
+
+            data.getWeather(city);
+        });
+    }
 })();
